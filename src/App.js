73175d6583416b04a92fac31b7334c2b4bed6e4a
Nizba-fathima <nizbafathima283@gmail.com>
2023-07-16 11:43:11 +0530

    edyoda assigmnet

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar'
+import Payment from './components/Payment';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <Navbar />
+      <Payment />
+    </div>
+  );
+}
+
+export default App;
